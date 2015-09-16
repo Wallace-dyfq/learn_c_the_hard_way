8ab02080f7ab9c0e1a826b2c830744140f6e38b7
wallace-dyfq <wallace.dyfq@gmail.com>
2015-09-16 06:45:22 -0600

    ex19

--- object.c
+++ object.c
@@ -13,8 +13,8 @@ void Object_destroy(void *self){
   Object *obj = self;
   if (obj) {
     if(obj->description) free(obj->description);
-    free(obj)
-      }
+    free(obj);
+   }
 }
 void Object_describe(void *self){
   Object *obj = self;
@@ -28,7 +28,7 @@ int Object_attack(void *self, int damage){
   printf("You cannot attack that.\n");
   return 0;
 }
-void *Object_new(size size, Object proto, char *description){
+void *Object_new(size_t size, Object proto, char *description){
   // setup the default functions in case they aren't set
   if (!proto.init) proto.init = Object_init;
   if (!proto.describe) proto.describe = Object_describe;
--- object.h
+++ object.h
@@ -1,5 +1,5 @@
-#ifndef_object_h
-#define_object_h
+#ifndef _object_h
+#define _object_h
 
 typedef enum{
   NORTH, SOUTH, EAST, WEST
@@ -7,11 +7,13 @@ typedef enum{
 
 typedef struct {
   char *description;
-  int (*init)(void *self);
-  void (*describe) (void *self);
-  void (*destroy)(void *self);
-  void *(*move)(void *self, Direction direction);
-  int (*attack)(void *self, int damage);
+  int (*init)(void *self);  // function object
+  void (*describe) (void *self);   // function object
+  void (*destroy)(void *self);   // function object
+  void *(*move)(void *self, Direction direction);   // function object
+
+  int (*attack)(void *self, int damage);    // function object
+
 } Object;
 
 int Object_init(void *self);
@@ -19,9 +21,9 @@ void Object_destroy(void *self);
 void Object_describe(void *self);
 void *Object_move(void *self, Direction direction);
 int Object_attack(void *self, int damage);
-void *Object_new(size size, Object proto, char *description);
+void *Object_new(size_t size, Object proto, char *description);
 
-#define NEW(T,N) Object_new(sizeof(T), T##proto,, N);
+#define NEW(T,N) Object_new(sizeof(T), T##Proto, N);
 #define _(N) proto.N
 
 #endif
